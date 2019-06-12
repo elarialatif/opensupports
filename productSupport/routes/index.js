ab0e526147d859e271b80521124d03041a5cb924
BadrAbed <badrabed66@gmail.com>
2019-06-12 12:01:26 +0200

    edit in views and routes

--- productSupport/routes/index.js
+++ productSupport/routes/index.js
@@ -2,11 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    if (req.user) {
+        res.redirect('/productSupport/userDashboard');
+    }
+    else {
+        res.render('index', {title: 'Express'});
+    }
+
 });
-router.get('/badr', function(req, res, next) {
- res.send("badr");
+router.get('/badr', function (req, res, next) {
+    res.send("badr");
 });
 
 module.exports = router;
